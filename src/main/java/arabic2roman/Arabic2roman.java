5264c23f02f437cdc8338ac54ae2b67650bd20d8
thomasAnd <atom.34@yahoo.com>
2022-07-13 15:28:40 +0200

    git init

---
+++ src/main/java/arabic2roman/Arabic2roman.java
@@ -0,0 +1,10 @@
+package arabic2roman;
+
+import java.io.PrintStream;
+import java.security.InvalidParameterException;
+
+public class Arabic2roman {
+
+
+
+}
---
+++ src/test/java/arabic2roman/Arabic2romanTest.java
@@ -0,0 +1,14 @@
+package arabic2roman;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Arabic2romanTest {
+
+
+
+}
